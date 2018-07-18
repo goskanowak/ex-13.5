e66d904721aee441c95ae3836b4681c62eafcabb
Małgorzata Nowak <goskanowak87@gmail.com>
2018-07-18 21:30:50 +0200

    Add modulo

--- modules/timeFormat.js
+++ modules/timeFormat.js
@@ -4,10 +4,10 @@ function calcTime() {
       let time = os.uptime();
       let hours = (time / 3600).toFixed(0);
       time = (time - (hours * 3600));
-      let minutes = (time / 60).toFixed(0);
-      let seconds = (time - minutes * 60).toFixed(0);
-      time = hours + 'h ' + minutes + 'min ' + seconds + 'sec ';
-      console.log(time);
+      let min = (time % 3600).toFixed(0);
+      let sec = (time - min * 60).toFixed(0);
+      timeAll = hours + 'h ' + min + 'min ' + sec + 'sec ';
+      console.log(timeAll);
 }
 
 exports.print = calcTime;
