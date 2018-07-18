edb75bb7f23dd5351683c11871014f0d98913d95
Małgorzata Nowak <goskanowak87@gmail.com>
2018-07-18 21:35:44 +0200

    Delete unnecesary variable

--- modules/timeFormat.js
+++ modules/timeFormat.js
@@ -3,9 +3,8 @@ let os = require('os');
 function calcTime() {
       let time = os.uptime();
       let hours = (time / 3600).toFixed(0);
-      time = (time - (hours * 3600));
-      let min = (time % 3600).toFixed(0);
-      let sec = (time - min * 60).toFixed(0);
+      let min = (time % 60).toFixed(0);
+      let sec = (time % 3600).toFixed(0);
       timeAll = hours + 'h ' + min + 'min ' + sec + 'sec ';
       console.log(timeAll);
 }
